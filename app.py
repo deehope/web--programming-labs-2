39b7e8c191fc559f03ce27cf95476cfb3111df53
Соколова Дарья <darya.sokolova2004@mail.ru>
2024-09-07 15:03:32 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,33 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>НГТУ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <ul>
+            <li><a href = "/lab1">Первая лабораторная<li>
+        <ul>
+
+        <footer>
+            &copy; Соколова Дарья, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+@app.route("/lab1")
+def lab1():
     return """
 <!DOCTYPE html>
 <html lang="ru">
@@ -17,6 +43,14 @@ def start():
         </header>
 
         <h1>web-сервер на flask</h1>
+
+        <p>Flask — фреймворк для создания веб-приложений на языке
+            программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+            называемых микрофреймворков — минималистичных каркасов
+
+            веб-приложений, сознательно предоставляющих лишь самые ба-
+            зовые возможности.<p>
 
         <footer>
             &copy; Соколова Дарья, ФБИ-23, 3 курс, 2024
