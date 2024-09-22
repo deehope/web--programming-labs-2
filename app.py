e342d10dcb907bc30f4c503f7af1dc5a9427b315
Соколова Дарья <darya.sokolova2004@mail.ru>
2024-09-22 17:55:17 +0700

    Книги

--- app.py
+++ app.py
@@ -216,4 +216,16 @@ def example():
         {'name':'манадарины', 'price': 95},
         {'name':'манго', 'price': 320}
     ]
-    return render_template('example.html', number = number, name = name, group = group, course = course, fruits = fruits)
+    books = [
+        {'author': 'Донна Тартт', 'name': 'Щегол', 'genre': 'роман', 'count': 828 },
+        {'author': 'Мария Метлицкая', 'name': 'Дом в Мансуровском', 'genre': 'роман', 'count': 340 },
+        {'author': 'Сергей Лукьяненко', 'name': 'Форсайт', 'genre': 'фантастика', 'count': 271 },
+        {'author': 'Ребекка Яррос', 'name': 'Железное пламя', 'genre': 'фэнтези', 'count': 953 },
+        {'author': 'Татьяна Корсакова', 'name': 'Марь', 'genre': 'мистика', 'count': 350 },
+        {'author': 'Роберт Чалдини', 'name': 'Психология влияния. 7-е расширенное издание', 'genre': 'детектив', 'count': 191 },
+        {'author': 'Дарья Донцова', 'name': 'Блоха на балу', 'genre': 'роман', 'count': 828 },
+        {'author': 'Мери Ли', 'name': 'Квента. Трилогия', 'genre': 'роман', 'count': 630 },
+        {'author': 'Дэвид Эллис', 'name': 'Дом лжи', 'genre': 'детектив', 'count': 426 },
+        {'author': 'Наталья Мамлеева', 'name': 'Злодейка своего романа', 'genre': 'фэнтези', 'count': 220 }
+    ]
+    return render_template('example.html', number = number, name = name, group = group, course = course, fruits = fruits, books = books)
