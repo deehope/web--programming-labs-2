eba43c17e4c4bf4118835cd1f3d2ab9bc358929c
Соколова Дарья <darya.sokolova2004@mail.ru>
2024-10-18 22:42:07 +0700

    Добавление неизвестный и аноним

--- lab3.py
+++ lab3.py
@@ -4,9 +4,10 @@ lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
 def lab():
-    name = request.cookies.get('name')
+    name = request.cookies.get('name') or 'аноним'
     name_color = request.cookies.get('name_color')
-    return render_template('lab3/lab3.html', name=name, name_color=name_color)
+    age = request.cookies.get('age') or 'неизвестный'
+    return render_template('lab3/lab3.html', name=name, name_color=name_color, age=age)
 
 @lab3.route('/lab3/cookie')
 def cookie():
@@ -105,5 +106,7 @@ def settings():
                                              fontweight=fontweight))
     
     return resp
+
+
 
     
