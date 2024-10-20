782b1bb632f2c242a267ee451dd1762787be7583
Соколова Дарья <darya.sokolova2004@mail.ru>
2024-10-20 14:32:07 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request
 lab4 = Blueprint('lab4', __name__)
 
+
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4/lab4.html')
@@ -9,6 +10,7 @@ def lab():
 @lab4.route('/lab4/div-form')
 def div_form():
     return render_template('lab4/div-form.html')
+
 
 @lab4.route('/lab4/div', methods=["POST"])
 def div():
@@ -28,3 +30,83 @@ def div():
 
     except ValueError:
         return render_template('lab4/div-form.html', error='Оба поля должны содержать числа')
+    
+
+@lab4.route('/lab4/add-form')
+def add_form():
+    return render_template('lab4/add-form.html')
+
+
+@lab4.route('/lab4/add', methods=["POST"])
+def add():
+    x1 = request.form.get('x1') or 0
+    x2 = request.form.get('x2') or 0
+
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 + x2
+    return render_template('lab4/add-form.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/mul-form')
+def mul_form():
+    return render_template('lab4/mul-form.html')
+
+
+@lab4.route('/lab4/mul', methods=["POST"])
+def mul():
+    x1 = request.form.get('x1') or 1
+    x2 = request.form.get('x2') or 1
+
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 * x2
+    return render_template('lab4/mul-form.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('lab4/sub-form.html')
+
+
+@lab4.route('/lab4/sub', methods=["POST"])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub-form.html', error='Оба поля должны быть заполнены')
+
+    try:
+        x1 = int(x1)
+        x2 = int(x2)
+        result = x1 - x2
+        return render_template('lab4/sub-form.html', x1=x1, x2=x2, result=result)
+    except ValueError:
+        return render_template('lab4/sub-form.html', error='Оба поля должны содержать числа')
+
+
+@lab4.route('/lab4/pow-form')
+def pow_form():
+    return render_template('lab4/pow-form.html')
+
+
+@lab4.route('/lab4/pow', methods=["POST"])
+def pow():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+
+    if x1 == '' or x2 == '':
+        return render_template('lab4/pow-form.html', error='Оба поля должны быть заполнены')
+
+    try:
+        x1 = int(x1)
+        x2 = int(x2)
+
+        if x1 == 0 and x2 == 0:
+            return render_template('lab4/pow-form.html', error='0 в степени 0 не определено')
+
+        result = x1 ** x2
+        return render_template('lab4/pow-form.html', x1=x1, x2=x2, result=result)
+    except ValueError:
+        return render_template('lab4/pow-form.html', error='Оба поля должны содержать числа')
