8792cebe6722b0ef2bce63245a3225d072a8dd07
Соколова Дарья <darya.sokolova2004@mail.ru>
2024-11-13 22:34:57 +0700

    Правки

--- lab5.py
+++ lab5.py
@@ -29,7 +29,7 @@ def db_connect():
         db_path = path.join(dir_path, "database.db")
         conn = sqlite3.connect(db_path)
         conn.row_factory = sqlite3.Row
-        cur = conn.cursor
+        cur = conn.cursor()
     return conn, cur
 
 
