08978e3a051dcdb89e12e58e0c7e72d210b732db
Соколова Дарья <darya.sokolova2004@mail.ru>
2024-11-15 18:57:31 +0700

    Правки

--- lab5.py
+++ lab5.py
@@ -174,7 +174,7 @@ def list():
     if current_app.config['DB_TYPE'] == 'postgres':
         cur.execute("SELECT * FROM articles WHERE user_id=%s ORDER BY is_favorite DESC;", (user_id,))
     else:
-        cur.execute("SELECT * FROM articles WHERE user_id=? ORDER BY is_favorite DESC;", (user_id,))
+        cur.execute("SELECT * FROM articles WHERE login_id=? ORDER BY is_favorite DESC;", (user_id,))
     
     articles = cur.fetchall()
 
