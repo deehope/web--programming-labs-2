2f38c783af284cf4210522a8fc43f3391920e2da
Соколова Дарья <darya.sokolova2004@mail.ru>
2024-11-16 12:49:33 +0700

    Заготовка к 6-ой лабораторной

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 app = Flask(__name__)
 
@@ -19,6 +20,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 @app.route("/")
 def start():
@@ -75,6 +77,7 @@ def menu():
             <li><a href = "/lab3">Третья лабораторная</a></li>
             <li><a href = "/lab4">Четвертая лабораторная</a></li>
             <li><a href = "/lab5">Пятая лабораторная</a></li>
+            <li><a href = "/lab6">Шестая лабораторная</a></li>
         </ul>
 
 
