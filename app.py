4760d6c8c6a6feee06e8aab3b708c1e49cab7cb4
Соколова Дарья <darya.sokolova2004@mail.ru>
2024-12-23 09:52:58 +0700

    доработка

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if app.config['DB_TYPE'] == 'postgres':
         f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
 else:
     dir_path = path.dirname(path.realpath(__file__))
-    db_path = path.jpin(dir_path, "sokolova_darya_orm.db")
+    db_path = path.join(dir_path, "sokolova_darya_orm.db")
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
 db.init_app(app)
